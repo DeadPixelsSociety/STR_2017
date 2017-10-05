8cbe4985305d130ed682ef25f4bfa9927e1945c3
Thomas <tom.pradal@aliceadsl.fr>
2017-10-05 02:19:56 +0200

    Added sfml sample code as base project to compile

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
-
 using namespace std;
+
+#include <SFML/Window.hpp>
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    sf::Window window;
+
+    window.create(sf::VideoMode(800, 600), "STR_2017");
+
+    // on fait tourner le programme jusqu'à ce que la fenêtre soit fermée
+    while (window.isOpen())
+    {
+        // on inspecte tous les évènements de la fenêtre qui ont été émis depuis la précédente itération
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // évènement "fermeture demandée" : on ferme la fenêtre
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+
     return 0;
 }
